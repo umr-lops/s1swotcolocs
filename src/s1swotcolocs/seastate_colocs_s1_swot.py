3d17124753929e1ac1f93120fccee7cd62b7d9a2
agrouaze <antoine.grouazel@ifremer.fr>
2026-03-16 14:02:03 +0000

    fix issue in unit test with h5netcdf

--- src/s1swotcolocs/seastate_colocs_s1_swot.py
+++ src/s1swotcolocs/seastate_colocs_s1_swot.py
@@ -500,11 +500,7 @@ def loop_on_each_sar_tiles(
 
 def save_sea_state_coloc_file(colocds, fpath_out, cpt):
     """
-
-    :param colocds:
-    :param fpath_out:
-    :param cpt:
-    :return:
+    Saves the dataset using the netcdf4 engine to avoid H5DSis_scale errors.
     """
     if os.path.exists(fpath_out):
         app_logger.info("remove the existing file")
@@ -513,9 +509,13 @@ def save_sea_state_coloc_file(colocds, fpath_out, cpt):
     else:
         app_logger.debug("file does not exist -> brand-new file on disk")
         cpt["new_file"] += 1
+        
     if not os.path.exists(os.path.dirname(fpath_out)):
-        os.makedirs(os.path.dirname(fpath_out), mode=0o775)
-    colocds.to_netcdf(fpath_out, engine="h5netcdf")
+        os.makedirs(os.path.dirname(fpath_out), mode=0o775, exist_ok=True)
+    
+    # CHANGE: Use engine="netcdf4" instead of "h5netcdf"
+    colocds.to_netcdf(fpath_out, engine="netcdf4")
+    
     os.chmod(fpath_out, 0o664)
     app_logger.info("coloc file created : %s", fpath_out)
     return cpt
